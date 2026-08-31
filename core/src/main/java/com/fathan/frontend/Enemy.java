4b6e808b672715093ac29b991a86353afe2edb42
NFathanR <najwanfathanr@gmail.com>
2026-08-31 15:32:16 +0700

    NajwanFathanRinandriCS1 - Init

---
+++ core/src/main/java/com/fathan/frontend/Enemy.java
@@ -0,0 +1,16 @@
+package com.fathan.frontend;
+
+public class Enemy {
+    public int HP, maxHP;
+    public String Name;
+}
+
+public void takeDamage(int damage) {
+    // 1. Kurangi hp sebesar nilai damage.
+
+    // 2. HP tidak boleh kurang dari 0.
+
+    // 3. Tampilkan HP saat ini dalam format: [EnemyName] took [damage] damage! HP: [currentHP]/[maxHP]
+
+    // 4. Jika HP mencapai 0, tampilkan bahwa Enemy telah dikalahkan dalam format: [EnemyName] was defeated!
+}
---
+++ core/src/main/java/com/fathan/frontend/Player.java
@@ -0,0 +1,27 @@
+package com.fathan.frontend;
+
+public class Player {
+    public int HP, Power, SpellCards;
+    public String Name;
+
+    Player (int InpHP,int InpPower,int InpSpellCards,String InpName) {
+        this.HP =InpHP;
+        this.Power = InpPower;
+        this.SpellCards = InpSpellCards;
+        this.Name = InpName;
+    }
+
+    public static void main(String[] args){
+        Player playerA = new Player (100, 20, 5. "Phainon");
+    }
+}
+
+public void takeDamage(int damage) {
+    // 1. Kurangi hp sebesar nilai damage.
+
+    // 2. HP tidak boleh bernilai negatif.
+
+    // 3. Jika HP masih lebih dari 0, tampilkan HP yang tersisa dalam format: [PlayerName] took [damage] damage! Remaining HP: [hp]
+
+    // 4. Jika HP menjadi 0, tampilkan pesan bahwa Player telah dikalahkan.
+}
